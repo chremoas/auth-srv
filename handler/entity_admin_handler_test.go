67b774a4060eab36378e46269ccb2fc33c1b395d
Matt Maurer <maurer.it@gmail.com>
2017-04-17 11:32:46 -0400

    Moved repo mocks and stubbed new service impl tests.

--- handler/entity_admin_handler_test.go
+++ handler/entity_admin_handler_test.go
@@ -1,1 +1,27 @@
 package handler
+
+import "testing"
+
+func TestEntityAdminHandler_AllianceUpdate(t *testing.T) {
+
+}
+
+func TestEntityAdminHandler_AllianceUpdate_WithAllianceDoesNotExist(t *testing.T) {
+
+}
+
+func TestEntityAdminHandler_CorporationUpdate(t *testing.T) {
+
+}
+
+func TestEntityAdminHandler_CorporationUpdate_WithCorporationDoesNotExist(t *testing.T) {
+
+}
+
+func TestEntityAdminHandler_CharacterUpdate(t *testing.T) {
+
+}
+
+func TestEntityAdminHandler_CharacterUpdate_WithCharacterDoesNotExist(t *testing.T) {
+
+}
--- handler/entity_query_handler_test.go
+++ handler/entity_query_handler_test.go
@@ -1,1 +1,15 @@
 package handler
+
+import "testing"
+
+func TestEntityQueryHandler_GetAlliances(t *testing.T) {
+
+}
+
+func TestEntityQueryHandler_GetCorporations(t *testing.T) {
+
+}
+
+func TestEntityQueryHandler_GetCharacters(t *testing.T) {
+
+}
