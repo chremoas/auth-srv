30806594234249ad2b9bb132796f18e2d4e92a38
Matt Maurer <maurer.it@gmail.com>
2017-04-18 16:04:06 -0400

    Corp and Character update endpoints tested and implemented.

--- handler/entity_admin_handler.go
+++ handler/entity_admin_handler.go
@@ -48,6 +48,7 @@ func (eah *EntityAdminHandler) AllianceUpdate(ctx context.Context, request *abae
 	if err != nil {
 		response.Success = false
 		response.ErrorText = "Error while saving: " + err.Error()
+		return nil
 	}
 
 	response.Success = true
@@ -55,9 +56,105 @@ func (eah *EntityAdminHandler) AllianceUpdate(ctx context.Context, request *abae
 }
 
 func (eah *EntityAdminHandler) CorporationUpdate(ctx context.Context, request *abaeve_auth.CoporationAdminRequest, response *abaeve_auth.EntityAdminResponse) error {
+	if request.Corporation == nil {
+		response.Success = false
+		response.ErrorText = "Invalid Corporation (nil)"
+		return nil
+	}
+
+	if request.Corporation.Id == 0 {
+		response.Success = false
+		response.ErrorText = "Invalid Corporation Id (0/nil)"
+		return nil
+	}
+
+	if len(request.Corporation.Name) == 0 {
+		response.Success = false
+		response.ErrorText = "Invalid Corporation Name (empty/nil)"
+		return nil
+	}
+
+	if len(request.Corporation.Ticker) == 0 {
+		response.Success = false
+		response.ErrorText = "Invalid Corporation Ticker (empty/nil)"
+		return nil
+	}
+
+	corporation := model.Corporation{
+		CorporationId:     request.Corporation.Id,
+		CorporationName:   request.Corporation.Name,
+		CorporationTicker: request.Corporation.Ticker,
+	}
+
+	if request.Corporation.AllianceId != 0 {
+		alliance := repository.AllianceRepo.FindByAllianceId(request.Corporation.AllianceId)
+		if alliance == nil {
+			response.Success = false
+			response.ErrorText = "Invalid Alliance Id, Alliance doesn't exist"
+			return nil
+		} else {
+			corporation.AllianceId = &request.Corporation.AllianceId
+		}
+	}
+
+	err := repository.CorporationRepo.Save(&corporation)
+
+	if err != nil {
+		response.Success = false
+		response.ErrorText = "Error while saving: " + err.Error()
+		return nil
+	}
+
+	response.Success = true
 	return nil
 }
 
 func (eah *EntityAdminHandler) CharacterUpdate(ctx context.Context, request *abaeve_auth.CharacterAdminRequest, response *abaeve_auth.EntityAdminResponse) error {
+	if request.Character == nil {
+		response.Success = false
+		response.ErrorText = "Invalid Character (nil)"
+		return nil
+	}
+
+	if request.Character.Id == 0 {
+		response.Success = false
+		response.ErrorText = "Invalid Character Id (0/nil)"
+		return nil
+	}
+
+	if len(request.Character.Name) == 0 {
+		response.Success = false
+		response.ErrorText = "Invalid Character Name (empty/nil)"
+		return nil
+	}
+
+	if request.Character.CorporationId == 0 {
+		response.Success = false
+		response.ErrorText = "Invalid Corporation Id (0/nil)"
+		return nil
+	}
+
+	corporation := repository.CorporationRepo.FindByCorporationId(request.Character.CorporationId)
+	if corporation == nil {
+		response.Success = false
+		response.ErrorText = "Invalid Corporation Id, Corporation doesn't exist"
+		return nil
+	}
+
+	character := model.Character{
+		CharacterId:   request.Character.Id,
+		CharacterName: request.Character.Name,
+		CorporationId: request.Character.CorporationId,
+	}
+
+	err := repository.CharacterRepo.Save(&character)
+
+	if err != nil {
+		response.Success = false
+		response.ErrorText = "Error while saving: " + err.Error()
+		return nil
+	}
+
+	response.Success = true
 	return nil
 }
--- handler/entity_admin_handler_test.go
+++ handler/entity_admin_handler_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"github.com/abaeve/auth-srv/model"
 	"github.com/abaeve/auth-srv/proto"
+	"github.com/golang/mock/gomock"
 	"golang.org/x/net/context"
 	"testing"
 )
@@ -229,11 +230,18 @@ func TestEntityAdminHandler_AllianceUpdate_WithSaveError(t *testing.T) {
 }
 
 func TestEntityAdminHandler_CorporationUpdate(t *testing.T) {
-	mockCtrl, _, _, _, mockCorpRepo, _, _, _ := SharedSetup(t)
+	mockCtrl, _, _, _, mockCorpRepo, mockAlliRepo, _, _ := SharedSetup(t)
 	defer mockCtrl.Finish()
 
 	allianceId := int64(1)
 
+	mockAlliRepo.EXPECT().FindByAllianceId(allianceId).Return(
+		&model.Alliance{
+			AllianceId:     allianceId,
+			AllianceName:   "Test Alliance",
+			AllianceTicker: "TSTA",
+		},
+	)
 	mockCorpRepo.EXPECT().Save(
 		&model.Corporation{
 			CorporationId:     int64(1),
@@ -265,12 +273,285 @@ func TestEntityAdminHandler_CorporationUpdate(t *testing.T) {
 	}
 }
 
-func TestEntityAdminHandler_CorporationUpdate_WithSaveError(t *testing.T) {
-	mockCtrl, _, _, _, mockCorpRepo, _, _, _ := SharedSetup(t)
+func TestEntityAdminHandler_CorporationUpdate_WithNoAlliance(t *testing.T) {
+	mockCtrl, _, _, _, mockCorpRepo, mockAlliRepo, _, _ := SharedSetup(t)
+	defer mockCtrl.Finish()
+
+	mockAlliRepo.EXPECT().FindByAllianceId(gomock.Any()).Times(0)
+	mockCorpRepo.EXPECT().Save(
+		&model.Corporation{
+			CorporationId:     int64(1),
+			CorporationName:   "Test Corporation",
+			CorporationTicker: "TSTC",
+		},
+	).Return(nil)
+
+	eah := EntityAdminHandler{}
+	request := abaeve_auth.CoporationAdminRequest{
+		Corporation: &abaeve_auth.Corporation{
+			Id:     int64(1),
+			Name:   "Test Corporation",
+			Ticker: "TSTC",
+		},
+	}
+	response := abaeve_auth.EntityAdminResponse{}
+
+	err := eah.CorporationUpdate(context.Background(), &request, &response)
+
+	if err != nil {
+		t.Errorf("Expected nil error but received: (%s)", err)
+	}
+
+	if !response.Success {
+		t.Error("Received unsuccessful when successful was expected")
+	}
+}
+
+func TestEntityAdminHandler_CorporationUpdate_WithInvalidCorp(t *testing.T) {
+	mockCtrl, _, _, _, mockCorpRepo, mockAlliRepo, _, _ := SharedSetup(t)
 	defer mockCtrl.Finish()
 
 	allianceId := int64(1)
 
+	mockAlliRepo.EXPECT().FindByAllianceId(allianceId).Return(
+		&model.Alliance{
+			AllianceId:     allianceId,
+			AllianceName:   "Test Alliance",
+			AllianceTicker: "TSTA",
+		},
+	).Times(0)
+	mockCorpRepo.EXPECT().Save(
+		&model.Corporation{
+			CorporationId:     int64(1),
+			CorporationName:   "Test Corporation",
+			CorporationTicker: "TSTC",
+			AllianceId:        &allianceId,
+		},
+	).Times(0)
+
+	eah := EntityAdminHandler{}
+	request := abaeve_auth.CoporationAdminRequest{}
+	response := abaeve_auth.EntityAdminResponse{}
+
+	err := eah.CorporationUpdate(context.Background(), &request, &response)
+
+	if err != nil {
+		t.Errorf("Expected nil error but received: (%s)", err)
+	}
+
+	if response.Success {
+		t.Error("Received successful when unsuccessful was expected")
+	}
+
+	expectedErrorText := "Invalid Corporation (nil)"
+
+	if response.ErrorText != expectedErrorText {
+		t.Errorf("Expected: (%s) but received: (%s)", expectedErrorText, response.ErrorText)
+	}
+}
+
+func TestEntityAdminHandler_CorporationUpdate_WithInvalidCorpId(t *testing.T) {
+	mockCtrl, _, _, _, mockCorpRepo, mockAlliRepo, _, _ := SharedSetup(t)
+	defer mockCtrl.Finish()
+
+	allianceId := int64(1)
+
+	mockAlliRepo.EXPECT().FindByAllianceId(allianceId).Return(
+		&model.Alliance{
+			AllianceId:     allianceId,
+			AllianceName:   "Test Alliance",
+			AllianceTicker: "TSTA",
+		},
+	).Times(0)
+	mockCorpRepo.EXPECT().Save(
+		&model.Corporation{
+			CorporationId:     int64(1),
+			CorporationName:   "Test Corporation",
+			CorporationTicker: "TSTC",
+			AllianceId:        &allianceId,
+		},
+	).Times(0)
+
+	eah := EntityAdminHandler{}
+	request := abaeve_auth.CoporationAdminRequest{
+		Corporation: &abaeve_auth.Corporation{
+			Name:       "Test Corporation",
+			Ticker:     "TSTC",
+			AllianceId: allianceId,
+		},
+	}
+	response := abaeve_auth.EntityAdminResponse{}
+
+	err := eah.CorporationUpdate(context.Background(), &request, &response)
+
+	if err != nil {
+		t.Errorf("Expected nil error but received: (%s)", err)
+	}
+
+	if response.Success {
+		t.Error("Received successful when unsuccessful was expected")
+	}
+
+	expectedErrorText := "Invalid Corporation Id (0/nil)"
+
+	if response.ErrorText != expectedErrorText {
+		t.Errorf("Expected: (%s) but received: (%s)", expectedErrorText, response.ErrorText)
+	}
+}
+
+func TestEntityAdminHandler_CorporationUpdate_WithInvalidCorpName(t *testing.T) {
+	mockCtrl, _, _, _, mockCorpRepo, mockAlliRepo, _, _ := SharedSetup(t)
+	defer mockCtrl.Finish()
+
+	allianceId := int64(1)
+
+	mockAlliRepo.EXPECT().FindByAllianceId(allianceId).Return(
+		&model.Alliance{
+			AllianceId:     allianceId,
+			AllianceName:   "Test Alliance",
+			AllianceTicker: "TSTA",
+		},
+	).Times(0)
+	mockCorpRepo.EXPECT().Save(
+		&model.Corporation{
+			CorporationId:     int64(1),
+			CorporationTicker: "TSTC",
+			AllianceId:        &allianceId,
+		},
+	).Times(0)
+
+	eah := EntityAdminHandler{}
+	request := abaeve_auth.CoporationAdminRequest{
+		Corporation: &abaeve_auth.Corporation{
+			Id:         int64(1),
+			Ticker:     "TSTC",
+			AllianceId: allianceId,
+		},
+	}
+	response := abaeve_auth.EntityAdminResponse{}
+
+	err := eah.CorporationUpdate(context.Background(), &request, &response)
+
+	if err != nil {
+		t.Errorf("Expected nil error but received: (%s)", err)
+	}
+
+	if response.Success {
+		t.Error("Received successful when unsuccessful was expected")
+	}
+
+	expectedErrorText := "Invalid Corporation Name (empty/nil)"
+
+	if response.ErrorText != expectedErrorText {
+		t.Errorf("Expected: (%s) but received: (%s)", expectedErrorText, response.ErrorText)
+	}
+}
+
+func TestEntityAdminHandler_CorporationUpdate_WithInvalidCorpTicker(t *testing.T) {
+	mockCtrl, _, _, _, mockCorpRepo, mockAlliRepo, _, _ := SharedSetup(t)
+	defer mockCtrl.Finish()
+
+	allianceId := int64(1)
+
+	mockAlliRepo.EXPECT().FindByAllianceId(allianceId).Return(
+		&model.Alliance{
+			AllianceId:     allianceId,
+			AllianceName:   "Test Alliance",
+			AllianceTicker: "TSTA",
+		},
+	).Times(0)
+	mockCorpRepo.EXPECT().Save(
+		&model.Corporation{
+			CorporationId:   int64(1),
+			CorporationName: "Test Corporation",
+			AllianceId:      &allianceId,
+		},
+	).Times(0)
+
+	eah := EntityAdminHandler{}
+	request := abaeve_auth.CoporationAdminRequest{
+		Corporation: &abaeve_auth.Corporation{
+			Id:         int64(1),
+			Name:       "Test Corporation",
+			AllianceId: allianceId,
+		},
+	}
+	response := abaeve_auth.EntityAdminResponse{}
+
+	err := eah.CorporationUpdate(context.Background(), &request, &response)
+
+	if err != nil {
+		t.Errorf("Expected nil error but received: (%s)", err)
+	}
+
+	if response.Success {
+		t.Error("Received successful when unsuccessful was expected")
+	}
+
+	expectedErrorText := "Invalid Corporation Ticker (empty/nil)"
+
+	if response.ErrorText != expectedErrorText {
+		t.Errorf("Expected: (%s) but received: (%s)", expectedErrorText, response.ErrorText)
+	}
+}
+
+func TestEntityAdminHandler_CorporationUpdate_WithNonExistingAlliance(t *testing.T) {
+	mockCtrl, _, _, _, mockCorpRepo, mockAlliRepo, _, _ := SharedSetup(t)
+	defer mockCtrl.Finish()
+
+	allianceId := int64(1)
+
+	mockAlliRepo.EXPECT().FindByAllianceId(allianceId).Return(nil)
+	mockCorpRepo.EXPECT().Save(
+		&model.Corporation{
+			CorporationId:     int64(1),
+			CorporationName:   "Test Corporation",
+			CorporationTicker: "TSTC",
+			AllianceId:        &allianceId,
+		},
+	).Times(0)
+
+	eah := EntityAdminHandler{}
+	request := abaeve_auth.CoporationAdminRequest{
+		Corporation: &abaeve_auth.Corporation{
+			Id:         int64(1),
+			Name:       "Test Corporation",
+			Ticker:     "TSTC",
+			AllianceId: allianceId,
+		},
+	}
+	response := abaeve_auth.EntityAdminResponse{}
+
+	err := eah.CorporationUpdate(context.Background(), &request, &response)
+
+	if err != nil {
+		t.Errorf("Expected nil error but received: (%s)", err)
+	}
+
+	if response.Success {
+		t.Error("Received successful when unsuccessful was expected")
+	}
+
+	expectedErrorText := "Invalid Alliance Id, Alliance doesn't exist"
+
+	if response.ErrorText != expectedErrorText {
+		t.Errorf("Expected: (%s) but received: (%s)", expectedErrorText, response.ErrorText)
+	}
+}
+
+func TestEntityAdminHandler_CorporationUpdate_WithSaveError(t *testing.T) {
+	mockCtrl, _, _, _, mockCorpRepo, mockAlliRepo, _, _ := SharedSetup(t)
+	defer mockCtrl.Finish()
+
+	allianceId := int64(1)
+
+	mockAlliRepo.EXPECT().FindByAllianceId(allianceId).Return(
+		&model.Alliance{
+			AllianceId:     allianceId,
+			AllianceName:   "Test Alliance",
+			AllianceTicker: "TSTA",
+		},
+	)
 	mockCorpRepo.EXPECT().Save(
 		&model.Corporation{
 			CorporationId:     int64(1),
@@ -300,12 +581,25 @@ func TestEntityAdminHandler_CorporationUpdate_WithSaveError(t *testing.T) {
 	if response.Success {
 		t.Error("Received successful when unsuccessful was expected")
 	}
+
+	expectedErrorText := "Error while saving: " + iamSorryDave
+
+	if response.ErrorText != expectedErrorText {
+		t.Errorf("Expected: (%s) but received: (%s)", expectedErrorText, response.ErrorText)
+	}
 }
 
 func TestEntityAdminHandler_CharacterUpdate(t *testing.T) {
-	mockCtrl, _, _, mockCharRepo, _, _, _, _ := SharedSetup(t)
+	mockCtrl, _, _, mockCharRepo, mockCorpRepo, _, _, _ := SharedSetup(t)
 	defer mockCtrl.Finish()
 
+	mockCorpRepo.EXPECT().FindByCorporationId(int64(1)).Return(
+		&model.Corporation{
+			CorporationId:     int64(1),
+			CorporationName:   "Test Corporation",
+			CorporationTicker: "TSTC",
+		},
+	)
 	mockCharRepo.EXPECT().Save(
 		&model.Character{
 			CharacterId:   int64(1),
@@ -318,7 +612,7 @@ func TestEntityAdminHandler_CharacterUpdate(t *testing.T) {
 	request := abaeve_auth.CharacterAdminRequest{
 		Character: &abaeve_auth.Character{
 			Id:            int64(1),
-			Name:          "Test Corporation",
+			Name:          "Test Character",
 			CorporationId: int64(1),
 		},
 	}
@@ -335,23 +629,70 @@ func TestEntityAdminHandler_CharacterUpdate(t *testing.T) {
 	}
 }
 
-func TestEntityAdminHandler_CharacterUpdate_WithSaveError(t *testing.T) {
-	mockCtrl, _, _, mockCharRepo, _, _, _, _ := SharedSetup(t)
+func TestEntityAdminHandler_CharacterUpdate_WithNoChar(t *testing.T) {
+	mockCtrl, _, _, mockCharRepo, mockCorpRepo, _, _, _ := SharedSetup(t)
 	defer mockCtrl.Finish()
 
-	mockCharRepo.EXPECT().Save(
-		&model.Alliance{
-			AllianceId:     int64(1),
-			AllianceName:   "Test Alliance",
-			AllianceTicker: "TSTA",
+	mockCorpRepo.EXPECT().FindByCorporationId(int64(1)).Return(
+		&model.Corporation{
+			CorporationId:     int64(1),
+			CorporationName:   "Test Corporation",
+			CorporationTicker: "TSTC",
 		},
-	).Return(errors.New(iamSorryDave))
+	).Times(0)
+	mockCharRepo.EXPECT().Save(
+		&model.Character{
+			CharacterId:   int64(1),
+			CharacterName: "Test Character",
+			CorporationId: int64(1),
+		},
+	).Times(0)
+
+	eah := EntityAdminHandler{}
+	request := abaeve_auth.CharacterAdminRequest{}
+	response := abaeve_auth.EntityAdminResponse{}
+
+	err := eah.CharacterUpdate(context.Background(), &request, &response)
+
+	if err != nil {
+		t.Errorf("Expected nil error but received: (%s)", err)
+	}
+
+	if response.Success {
+		t.Error("Received successful when unsuccessful was expected")
+	}
+
+	expectedErrorText := "Invalid Character (nil)"
+
+	if response.ErrorText != expectedErrorText {
+		t.Errorf("Expected: (%s) but received: (%s)", expectedErrorText, response.ErrorText)
+	}
+}
+
+func TestEntityAdminHandler_CharacterUpdate_WithInvalidCharId(t *testing.T) {
+	mockCtrl, _, _, mockCharRepo, mockCorpRepo, _, _, _ := SharedSetup(t)
+	defer mockCtrl.Finish()
+
+	mockCorpRepo.EXPECT().FindByCorporationId(int64(1)).Return(
+		&model.Corporation{
+			CorporationId:     int64(1),
+			CorporationName:   "Test Corporation",
+			CorporationTicker: "TSTC",
+		},
+	).Times(0)
+	mockCharRepo.EXPECT().Save(
+		&model.Character{
+			CharacterId:   int64(1),
+			CharacterName: "Test Character",
+			CorporationId: int64(1),
+		},
+	).Times(0)
 
 	eah := EntityAdminHandler{}
 	request := abaeve_auth.CharacterAdminRequest{
 		Character: &abaeve_auth.Character{
-			Id:   int64(1),
-			Name: "Test Corporation",
+			Name:          "Test Character",
+			CorporationId: int64(1),
 		},
 	}
 	response := abaeve_auth.EntityAdminResponse{}
@@ -364,5 +705,233 @@ func TestEntityAdminHandler_CharacterUpdate_WithSaveError(t *testing.T) {
 
 	if response.Success {
 		t.Error("Received successful when unsuccessful was expected")
+	}
+
+	expectedErrorText := "Invalid Character Id (0/nil)"
+
+	if response.ErrorText != expectedErrorText {
+		t.Errorf("Expected: (%s) but received: (%s)", expectedErrorText, response.ErrorText)
+	}
+}
+
+func TestEntityAdminHandler_CharacterUpdate_WithInvalidCharName(t *testing.T) {
+	mockCtrl, _, _, mockCharRepo, mockCorpRepo, _, _, _ := SharedSetup(t)
+	defer mockCtrl.Finish()
+
+	mockCorpRepo.EXPECT().FindByCorporationId(int64(1)).Return(
+		&model.Corporation{
+			CorporationId:     int64(1),
+			CorporationName:   "Test Corporation",
+			CorporationTicker: "TSTC",
+		},
+	).Times(0)
+	mockCharRepo.EXPECT().Save(
+		&model.Character{
+			CharacterId:   int64(1),
+			CharacterName: "Test Character",
+			CorporationId: int64(1),
+		},
+	).Times(0)
+
+	eah := EntityAdminHandler{}
+	request := abaeve_auth.CharacterAdminRequest{
+		Character: &abaeve_auth.Character{
+			Id:            int64(1),
+			CorporationId: int64(1),
+		},
+	}
+	response := abaeve_auth.EntityAdminResponse{}
+
+	err := eah.CharacterUpdate(context.Background(), &request, &response)
+
+	if err != nil {
+		t.Errorf("Expected nil error but received: (%s)", err)
+	}
+
+	if response.Success {
+		t.Error("Received successful when unsuccessful was expected")
+	}
+
+	expectedErrorText := "Invalid Character Name (empty/nil)"
+
+	if response.ErrorText != expectedErrorText {
+		t.Errorf("Expected: (%s) but received: (%s)", expectedErrorText, response.ErrorText)
+	}
+}
+
+func TestEntityAdminHandler_CharacterUpdate_WithInvalidCorpId(t *testing.T) {
+	mockCtrl, _, _, mockCharRepo, mockCorpRepo, _, _, _ := SharedSetup(t)
+	defer mockCtrl.Finish()
+
+	mockCorpRepo.EXPECT().FindByCorporationId(int64(1)).Return(
+		&model.Corporation{
+			CorporationId:     int64(1),
+			CorporationName:   "Test Corporation",
+			CorporationTicker: "TSTC",
+		},
+	).Times(0)
+	mockCharRepo.EXPECT().Save(
+		&model.Character{
+			CharacterId:   int64(1),
+			CharacterName: "Test Character",
+			CorporationId: int64(0),
+		},
+	).Times(0)
+
+	eah := EntityAdminHandler{}
+	request := abaeve_auth.CharacterAdminRequest{
+		Character: &abaeve_auth.Character{
+			Id:            int64(1),
+			Name:          "Test Character",
+			CorporationId: int64(0),
+		},
+	}
+	response := abaeve_auth.EntityAdminResponse{}
+
+	err := eah.CharacterUpdate(context.Background(), &request, &response)
+
+	if err != nil {
+		t.Errorf("Expected nil error but received: (%s)", err)
+	}
+
+	if response.Success {
+		t.Error("Received successful when unsuccessful was expected")
+	}
+
+	expectedErrorText := "Invalid Corporation Id (0/nil)"
+
+	if response.ErrorText != expectedErrorText {
+		t.Errorf("Expected: (%s) but received: (%s)", expectedErrorText, response.ErrorText)
+	}
+}
+
+func TestEntityAdminHandler_CharacterUpdate_WithCorpIdNotSet(t *testing.T) {
+	mockCtrl, _, _, mockCharRepo, mockCorpRepo, _, _, _ := SharedSetup(t)
+	defer mockCtrl.Finish()
+
+	mockCorpRepo.EXPECT().FindByCorporationId(int64(1)).Return(
+		&model.Corporation{
+			CorporationId:     int64(1),
+			CorporationName:   "Test Corporation",
+			CorporationTicker: "TSTC",
+		},
+	).Times(0)
+	mockCharRepo.EXPECT().Save(
+		&model.Character{
+			CharacterId:   int64(1),
+			CharacterName: "Test Character",
+			CorporationId: int64(0),
+		},
+	).Times(0)
+
+	eah := EntityAdminHandler{}
+	request := abaeve_auth.CharacterAdminRequest{
+		Character: &abaeve_auth.Character{
+			Id:   int64(1),
+			Name: "Test Character",
+		},
+	}
+	response := abaeve_auth.EntityAdminResponse{}
+
+	err := eah.CharacterUpdate(context.Background(), &request, &response)
+
+	if err != nil {
+		t.Errorf("Expected nil error but received: (%s)", err)
+	}
+
+	if response.Success {
+		t.Error("Received successful when unsuccessful was expected")
+	}
+
+	expectedErrorText := "Invalid Corporation Id (0/nil)"
+
+	if response.ErrorText != expectedErrorText {
+		t.Errorf("Expected: (%s) but received: (%s)", expectedErrorText, response.ErrorText)
+	}
+}
+
+func TestEntityAdminHandler_CharacterUpdate_WithNonExistingCorp(t *testing.T) {
+	mockCtrl, _, _, mockCharRepo, mockCorpRepo, _, _, _ := SharedSetup(t)
+	defer mockCtrl.Finish()
+
+	mockCorpRepo.EXPECT().FindByCorporationId(int64(1)).Return(nil)
+	mockCharRepo.EXPECT().Save(
+		&model.Character{
+			CharacterId:   int64(1),
+			CharacterName: "Test Character",
+			CorporationId: int64(0),
+		},
+	).Times(0)
+
+	eah := EntityAdminHandler{}
+	request := abaeve_auth.CharacterAdminRequest{
+		Character: &abaeve_auth.Character{
+			Id:            int64(1),
+			Name:          "Test Character",
+			CorporationId: int64(1),
+		},
+	}
+	response := abaeve_auth.EntityAdminResponse{}
+
+	err := eah.CharacterUpdate(context.Background(), &request, &response)
+
+	if err != nil {
+		t.Errorf("Expected nil error but received: (%s)", err)
+	}
+
+	if response.Success {
+		t.Error("Received successful when unsuccessful was expected")
+	}
+
+	expectedErrorText := "Invalid Corporation Id, Corporation doesn't exist"
+
+	if response.ErrorText != expectedErrorText {
+		t.Errorf("Expected: (%s) but received: (%s)", expectedErrorText, response.ErrorText)
+	}
+}
+
+func TestEntityAdminHandler_CharacterUpdate_WithSaveError(t *testing.T) {
+	mockCtrl, _, _, mockCharRepo, mockCorpRepo, _, _, _ := SharedSetup(t)
+	defer mockCtrl.Finish()
+
+	mockCorpRepo.EXPECT().FindByCorporationId(int64(1)).Return(
+		&model.Corporation{
+			CorporationId:     int64(1),
+			CorporationName:   "Test Corporation",
+			CorporationTicker: "TSTC",
+		},
+	)
+	mockCharRepo.EXPECT().Save(
+		&model.Character{
+			CharacterId:   int64(1),
+			CharacterName: "Test Character",
+			CorporationId: int64(1),
+		},
+	).Return(errors.New(iamSorryDave))
+
+	eah := EntityAdminHandler{}
+	request := abaeve_auth.CharacterAdminRequest{
+		Character: &abaeve_auth.Character{
+			Id:            int64(1),
+			Name:          "Test Character",
+			CorporationId: int64(1),
+		},
+	}
+	response := abaeve_auth.EntityAdminResponse{}
+
+	err := eah.CharacterUpdate(context.Background(), &request, &response)
+
+	if err != nil {
+		t.Errorf("Expected nil error but received: (%s)", err)
+	}
+
+	if response.Success {
+		t.Error("Received successful when unsuccessful was expected")
+	}
+
+	expectedErrorText := "Error while saving: " + iamSorryDave
+
+	if response.ErrorText != expectedErrorText {
+		t.Errorf("Expected: (%s) but received: (%s)", expectedErrorText, response.ErrorText)
 	}
 }
