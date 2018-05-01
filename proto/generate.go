168222d407f6faa4de1c7ec4faa8d1934d3a4607
Brian Hechinger <wonko@4amlunch.net>
2018-05-01 09:44:11 -0400

    Updated generated go from protobuf. Also updated generate.go for each service.

--- proto/generate.go
+++ proto/generate.go
@@ -1,6 +1,3 @@
 package abaeve_auth
 
-//go:generate pegomock generate --output=mocks/mock_userauthenticationclient.go --generate-matchers --package=authsrv_mocks UserAuthenticationClient
-//go:generate pegomock generate --output=mocks/mock_entityqueryclient.go --generate-matchers --package=authsrv_mocks EntityQueryClient
-//go:generate pegomock generate --output=mocks/mock_userauthenticationadminclient.go --generate-matchers --package=authsrv_mocks UserAuthenticationAdminClient
-//go:generate pegomock generate --output=mocks/mock_entityadminclient.go --generate-matchers --package=authsrv_mocks EntityAdminClient
+//go:generate sh -c "protoc --proto_path=$GOPATH/src:. --micro_out=. --go_out=. *.proto"
